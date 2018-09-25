b55cee2423e70c91cd9c6fb2a9cd5f158f1e8e47
Jacqueline Gaherity <v-jagahe@microsoft.com>
2018-09-24 22:16:56 -0700

    we got 2 api's yall

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,20 +4,29 @@
 // /v1/artists/{id}/top-tracks
 //34b6f34212c44af5befda2997f53a0bc
 
-$.ajax({
-    url: "https://api.spotify.com/v1/albums/",
-    method: "GET"
+// $.ajax({
+//   url: "https://api.spotify.com/v1/albums/",
+//   method: "GET"
+// }).then(function(response) {
+//   console.log(response);
+// });
+
+// Example queryURL for Genius API
+var queryURL = "https://api.genius.com/artists/16775?access_token=YCm2sHVhLP7PQ1szB3B4LLJqsksJEA3NqwuY_lIw12BoNGUVjkLem6M7yCSBHHeC"
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    Authorization: "Bearer YCm2sHVhLP7PQ1szB3B4LLJqsksJEA3NqwuY_lIw12BoNGUVjkLem6M7yCSBHHeC"
   }).then(function(response) {
     console.log(response);
-  });
+});
 
-      // Example queryURL for Genius API
-      var queryURL = "https://api.genius.com/artists/16775?access_token=YCm2sHVhLP7PQ1szB3B4LLJqsksJEA3NqwuY_lIw12BoNGUVjkLem6M7yCSBHHeC"
-
-      $.ajax({
-        url: queryURL,
-        method: "GET",
-        Authorization: "Bearer YCm2sHVhLP7PQ1szB3B4LLJqsksJEA3NqwuY_lIw12BoNGUVjkLem6M7yCSBHHeC"
-      }).then(function(response) {
-        console.log(response);
-      });
+var napster = "https://api.napster.com/v2.2/artists/top";
+$.ajax({
+  url: napster,
+  method: "GET",
+  apikey: "YzgwY2IxYmQtY2JmMC00MTcwLWJiZDYtOTUyMTZhMTlhNDRl",
+}).then(function(response) {
+  console.log({response});
+});
