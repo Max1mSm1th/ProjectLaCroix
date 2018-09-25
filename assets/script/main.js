7b85de7958a9229c299718a39cb6cf9c68ba32f0
Lewis Gregerson-Spencer <lewisgregerson-spencer@D-10-19-193-142.dhcp4.washington.edu>
2018-09-24 19:10:54 -0700

    test

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,12 @@
-//main.js file
+// https://api.spotify.com/v1
+
+// /v1/artists/{id}
+// /v1/artists/{id}/top-tracks
+//34b6f34212c44af5befda2997f53a0bc
+
+$.ajax({
+    url: "https://api.spotify.com/v1/albums/",
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
