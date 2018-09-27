970d0586bcf4463629f550b39e866cad0f22d70a
Vanita <vanitasharma_16@hotmail.com>
2018-09-26 20:36:57 -0700

    Genius search API

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,23 +1,10 @@
-// https://api.spotify.com/v1
-
-// /v1/artists/{id}
-// /v1/artists/{id}/top-tracks
-//34b6f34212c44af5befda2997f53a0bc
-
-$.ajax({
-    url: "https://api.spotify.com/v1/albums/",
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
-
-      // Example queryURL for Genius API
-      var queryURL = "https://api.genius.com/artists/16775?access_token=YCm2sHVhLP7PQ1szB3B4LLJqsksJEA3NqwuY_lIw12BoNGUVjkLem6M7yCSBHHeC"
+    var artist = "hello its me"
+      var queryURL = `https://api.genius.com/search?q=${artist}&access_token=cFZv8YyIQLCciFlaZn2Q0nQL-8szl5ES5jHqFo2kyfrheVEmDKen-PBxWgQ0dRVI`;
 
       $.ajax({
         url: queryURL,
         method: "GET",
-        Authorization: "Bearer YCm2sHVhLP7PQ1szB3B4LLJqsksJEA3NqwuY_lIw12BoNGUVjkLem6M7yCSBHHeC"
+  
       }).then(function(response) {
         console.log(response);
       });
